3a6d99facb5335375bb6bfd7625610be5fbfdcb5
Inori Yuzuriha <drawangel1234@yahoo.com>
2019-05-02 06:39:35 +0800

    Changed axios base url

--- src/axios.js
+++ src/axios.js
@@ -1,3 +1,3 @@
 window.axios = require('axios');
-window.axios.defaults.baseURL = 'http://localhost/feed-analysis-api/public'
+window.axios.defaults.baseURL = 'https://feed-analysis-api.herokuapp.com/'
 window.axios.defaults.headers.common = {'Accept': "application/json"}
