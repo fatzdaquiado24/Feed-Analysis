895d80927d4d862cab0d753ba1d3d113f4142e7b
Inori Yuzuriha <drawangel1234@yahoo.com>
2019-05-03 12:10:34 +0800

    Test

--- src/views/ClientOurOfficeLocation.vue
+++ src/views/ClientOurOfficeLocation.vue
@@ -7,12 +7,12 @@
     <section>
       <v-container>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3925.3348376950994!2d123.88519731471949!3d10.315065170360427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a9994b05612cdf%3A0x43ae638feefe1187!2sDepartment+Of+Agriculture!5e0!3m2!1sen!2sph!4v1556856462360!5m2!1sen!2sph"
-          width="600"
-          frameborder="0"
-          style="border:0;width:100%"
-          allowfullscreen
-        ></iframe>
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3925.3348376950994!2d123.88519731471949!3d10.315065170360427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a9994b05612cdf%3A0x43ae638feefe1187!2sDepartment+Of+Agriculture!5e0!3m2!1sen!2sph!4v1556856462360!5m2!1sen!2sph"
+              height="450"
+              frameborder="0"
+              style="border:0;width:100%"
+              allowfullscreen
+            ></iframe>
       </v-container>
     </section>
   </div>
