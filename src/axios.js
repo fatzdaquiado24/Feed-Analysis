3a774624428e97ae30acba8092a2723999dae4a9
Charles Salinas <salinasandrei45@gmail.com>
2019-05-18 20:18:55 +0800

    Test

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,3 @@
 window.axios = require('axios');
-// window.axios.defaults.baseURL = 'http://feed-analysis-api.herokuapp.com/'
-window.axios.defaults.baseURL = 'http://localhost/feed-analysis-api/public'
+window.axios.defaults.baseURL = 'http://feed-analysis-api.herokuapp.com/'
 window.axios.defaults.headers.common = {'Accept': "application/json"}
